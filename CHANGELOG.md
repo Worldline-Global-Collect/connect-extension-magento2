5aaf4f0490530a04c9ed62df3e2629fbc7bce3b9
WorldlineConnect <github.connect@worldline.com>
2025-03-13 10:01:27 +0100

    Release 4.7.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## 4.7.0 - 2025-03-13
+
+- Fix missing order summary content in invoice emails
+
 ## 4.6.0 - 2025-02-28
 
 - Fix emails sending when direct capture is configured, but customer is not redirected to the shop
--- Model/Worldline/EmailSender.php
+++ Model/Worldline/EmailSender.php
@@ -2,11 +2,13 @@
 
 namespace Worldline\Connect\Model\Worldline;
 
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Event;
 use Magento\Framework\Event\Observer;
 use Magento\Quote\Model\Quote;
 use Magento\Quote\Observer\SendInvoiceEmailObserver;
 use Magento\Quote\Observer\SubmitObserver;
+use Magento\Sales\Api\InvoiceRepositoryInterface;
 use Magento\Sales\Model\Order;
 
 class EmailSender
@@ -19,15 +21,21 @@ class EmailSender
      * @var SubmitObserver
      */
     private $submitObserver;
+    /**
+     * @var InvoiceRepositoryInterface
+     */
+    private $invoiceRepository;
 
     /**
      * @param SendInvoiceEmailObserver $sendInvoiceEmailObserver
      * @param SubmitObserver $submitObserver
      */
-    public function __construct(SendInvoiceEmailObserver $sendInvoiceEmailObserver, SubmitObserver $submitObserver)
+    public function __construct(SendInvoiceEmailObserver $sendInvoiceEmailObserver, SubmitObserver $submitObserver,
+                                InvoiceRepositoryInterface $invoiceRepository)
     {
         $this->sendInvoiceEmailObserver = $sendInvoiceEmailObserver;
         $this->submitObserver = $submitObserver;
+        $this->invoiceRepository = $invoiceRepository;
     }
 
     public function sendEmails(Order $order, Quote $quote): void
@@ -39,6 +47,12 @@ class EmailSender
             'quote' => $quote
         ]);
         $observer->setEvent($event);
+
+        $invoice = current($order->getInvoiceCollection()->getItems());
+
+        if ($invoice) {
+            $this->invoiceRepository->save($invoice);
+        }
 
         $this->submitObserver->execute($observer);
         $this->sendInvoiceEmailObserver->execute($observer);
